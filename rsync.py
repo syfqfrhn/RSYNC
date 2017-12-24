91def522d3ec35852caa24389910c376f1be265b
syfqfrhn <syfqfrhn46@gmail.com>
2017-12-24 09:10:36 +0800

    rsync.py for push data rsync dor pull data

---
+++ rsync.py
@@ -0,0 +1,2 @@
+import os
+os.system("rsync -av 192.168.1.12:/home/syfqfrhn/Testing/file.txt /home/syfqfrhn/Testing2/")
---
+++ rsync2.py
@@ -0,0 +1,2 @@
+import os
+os.system("rsync /home/syfqfrhn/Testing2/file3.docx syfqfrhn@192.168.1.12:/home/syfqfrhn/Testing")
